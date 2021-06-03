ce27b95fed462df85797b3766a28a533a6bac390
zengyuanman <zengyuanman123@163.com>
2021-06-03 23:57:21 +0800

    添加git排除

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# template-cli-ma
+# weapp
 
-vuecli3配置好的脚手架
 
 ``` js
-npm i template-cli-ma  -g 
+npm i weapp  -g 
 
 ```
 
 ``` js
 //查看版本号
-template-ma -v 
+weapp -v 
 ```
 
 ``` js
 //创建项目  目标文件夹中使用 
-template-ma init <项目名>
+
+weapp init <项目名>
 
 ```
+
+
+
+使用vant-weapp 作为ui框架，并对表单样式进行了修改
--- commands/init.js
+++ commands/init.js
@@ -2,13 +2,13 @@
 const shell = require('shelljs');
 const symbols = require('log-symbols');
 const clone = require('../utils/clone.js');
-const remote = 'https://github.com/MaLunan/components.git';
+const remote = 'https://github.com/zengyuanman/weapp-template.git';
 const fs =require('fs')
 const ora = require('ora'); // 用于输出loading
 const chalk = require('chalk'); // 用于改变文字颜色
 const notifier =require('node-notifier')
 const path = require('path')
-let branch = 'master';
+let branch = 'main';
 
 const initAction = async (name, option) => {
     // console.log("initAction -> option", option)
@@ -32,71 +32,71 @@ const initAction = async (name, option) => {
     await clone(`direct:${remote}#${branch}`, name, { clone: true });
 
     // 5. 清理文件
-    const deleteDir = ['.git', '.gitignore', 'README.md', 'docs']; // 需要清理的文件
+    const deleteDir = ['.git', '.gitignore', 'README.md']; // 需要清理的文件
     const pwd = shell.pwd();
     deleteDir.map(item => shell.rm('-rf', pwd + `/${name}/${item}`));
     const inquirer = require('inquirer');
     // 定义需要询问的问题
-    const questions = [
-    {
-        type: 'input',
-        message: '请输入模板名称:',
-        name: 'name',
-        validate(val) {
-        if (!val) return '模板名称不能为空！';
-        if (val.match(/[^A-Za-z0-9\u4e00-\u9fa5_-]/g)) return '模板名称包含非法字符，请重新输入';
-        return true;
-        }
-    },
-    {
-        type: 'input',
-        message: '请输入模板关键词（;分割）:',
-        name: 'keywords'
-    },
-    {
-        type: 'input',
-        message: '请输入模板简介:',
-        name: 'description'
-    },
-    {
-        type: 'input',
-        message: '请输入您的名字:',
-        name: 'author'
-    }
-    ];
-    // 通过inquirer获取到用户输入的内容
-    const answers = await inquirer.prompt(questions);
+    // const questions = [
+    // {
+    //     type: 'input',
+    //     message: '请输入模板名称:',
+    //     name: 'name',
+    //     validate(val) {
+    //     if (!val) return '模板名称不能为空！';
+    //     if (val.match(/[^A-Za-z0-9\u4e00-\u9fa5_-]/g)) return '模板名称包含非法字符，请重新输入';
+    //     return true;
+    //     }
+    // },
+    // {
+    //     type: 'input',
+    //     message: '请输入模板关键词（;分割）:',
+    //     name: 'keywords'
+    // },
+    // {
+    //     type: 'input',
+    //     message: '请输入模板简介:',
+    //     name: 'description'
+    // },
+    // {
+    //     type: 'input',
+    //     message: '请输入您的名字:',
+    //     name: 'author'
+    // }
+    // ];
+    // // 通过inquirer获取到用户输入的内容
+    // const answers = await inquirer.prompt(questions);
     // 将用户的配置打印，确认一下是否正确
     console.log('------------------------1',name,path.join(__dirname,'./'));
-    console.log(answers);
+    // console.log(answers);
 
-    let confirm = await inquirer.prompt([
-        {
-            type: 'confirm',
-            message: '确认创建？',
-            default: 'Y',
-            name: 'isConfirm'
-        }
-    ]);
-    if (!confirm.isConfirm) return false;
+    // let confirm = await inquirer.prompt([
+    //     {
+    //         type: 'confirm',
+    //         message: '确认创建？',
+    //         default: 'Y',
+    //         name: 'isConfirm'
+    //     }
+    // ]);
+    // if (!confirm.isConfirm) return false;
     //根据用户配置调整文件
-   let jsonData= fs.readFileSync(  `${name}/package.json`,function(err,data){
-       console.log(err)
-    })
-    jsonData=JSON.parse(jsonData)
-    for(item in answers){
-        jsonData[item]=answers[item]
-    }
-    console.log(jsonData)
-    console.log(path.join(__dirname,`${name}/`))
-    let obj=JSON.stringify(jsonData,null,'\t')
-    let sss=fs.writeFileSync(  `${name}/package.json`,obj,function(err,data){
-        console.log(err,data)
-    })
+  //  let jsonData= fs.readFileSync(  `${name}/package.json`,function(err,data){
+  //      console.log(err)
+  //   })
+  //   jsonData=JSON.parse(jsonData)
+  //   for(item in answers){
+  //       jsonData[item]=answers[item]
+  //   }
+  //   console.log(jsonData)
+  //   console.log(path.join(__dirname,`${name}/`))
+  //   let obj=JSON.stringify(jsonData,null,'\t')
+  //    fs.writeFileSync(  `${name}/package.json`,obj,function(err,data){
+  //       console.log(err,data)
+  //   })
     //自动安装依赖
     const installSpinner = ora('正在安装依赖...').start();
     shell.cd(name)
-    if (shell.exec('npm install').code !== 0) {
+    if (shell.exec('yarn').code !== 0) {
         console.log(symbols.warning, chalk.yellow('自动安装失败，请手动安装！'));
         installSpinner.fail(); // 安装失败
         shell.exit(1);
